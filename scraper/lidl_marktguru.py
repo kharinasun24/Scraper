e2682ac88e2aa5713c047f6dc0d5c14a027fc863
a.goelz <belsharuzzur@gmail.com>
2026-07-06 13:39:42 +0200

    Starting lidl scraper.

---
+++ scraper/lidl_marktguru.py
@@ -0,0 +1,17 @@
+# TODO: Implement this!
+from core.base import BaseScraper
+from bs4 import BeautifulSoup
+
+
+#Imports
+
+"""
+class LidlMarktguruScraper(BaseScraper):
+    def __init__(self):
+        super().__init__("https://www.marktguru.de/provider/lidl")
+
+    def extract_logic(self, page, kategorie: str):
+        log.info(f"Scrape Lidl-Preise über Marktguru für: {kategorie}")
+
+        if self.ist_stoppwort(produkt_name, kategorie):
+"""
